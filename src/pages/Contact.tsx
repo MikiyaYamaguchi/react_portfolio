9d4eb749a651ff7c340cf4f605334d0b3be72a7c
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2024-11-20 08:14:41 +0900

    フォームにnetlify用コードの追加

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -75,6 +75,7 @@ const Contact = () => {
             data-netlify="true"
             netlify-honeypot="bot-field"
           >
+            <input type="hidden" name="form-name" value="contact" />
             <div className="row">
               <div className="col span_3">
                 <label htmlFor="name">お名前</label>
